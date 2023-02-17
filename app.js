375da2f1f2330a28048c02ba5a59d76f68ba1865
MityaSesaro <mitiasecer1382@gmail.com>
2023-02-17 13:22:37 +0600

    Version 1.7

--- app.js
+++
@@ -1,5 +0,0 @@
-if (typeof navigator.serviceWorker !== 'undefined') {
-    navigator.serviceWorker.register('sw.js')
-        .then(() => console.log('Успешно'))
-        .catch(() => console.log('Ошибка'))
-}
